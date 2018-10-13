ce08f132e3a923b082ace648efda6358eb6e7187
cuongdqpayment <cuongdqpayment@gmail.com>
2018-10-13 20:16:27 +0700

    server js

--- middle.js
+++ middle.js
@@ -7,14 +7,41 @@ app.set("views", "./views");
 
 
 var server=require("http").Server(app);
+
 //yeu cau thu vien socket io
 var io = require("socket.io")(server);
+
+function isValid(tk) {
+    if (tk=="cuongdq"){
+        return true;
+        
+    }else{
+        return false;
+
+    }
+}
+
+// middleware
+io.use((socket, next) => {
+    //chuoi handshake.query la sau dau ?
+    let token = socket.handshake.query.token;
+
+    if (isValid(token)) {
+      return next();
+    }
+    return next(new Error('authentication error'));
+  });
+  
+
 server.listen(3000);
 
 //lang nghe client ket noi len server
 io.on("connection", function(socket){
     console.log("co nguoi ket noi len server: " + socket.id);  
     
+    let token = socket.handshake.query.token;
+    console.log("voi token = " + token);  
+
     socket.on("disconnect",function(){
         console.log("Ngat ket noi: " + socket.id);  
 
@@ -24,6 +51,7 @@ io.on("connection", function(socket){
 app.get("/", function(req,res){
     res.render("trangchu");
 });
+
 app.get("/test", function(req,res){
     res.render("trangcheck");
 });
---
+++ socket.js
@@ -0,0 +1,34 @@
+let app = require('express')();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+
+var port = process.env.PORT || 3001;
+
+http.listen(port, function () {
+    console.log('listening in http://localhost:' + port);
+});
+
+
+io.on('connection', (socket) => {
+    //lay dia chi ip cua may tram
+    var address = socket.handshake.address;
+    console.log('New connection from: ' + socket.id + ' ' + JSON.stringify(socket.request.connection._peername));
+    
+    socket.on('disconnect', function () {
+        console.log('disconnect from: ' + socket.id + ' ' + JSON.stringify(socket.request.connection._peername));
+        io.emit('users-changed', { user: socket.nickname, event: 'left' });
+    });
+    
+
+    socket.on('set-nickname', (nickname) => {
+        socket.nickname = nickname;
+        io.emit('users-changed', { user: nickname, event: 'joined' });
+    });
+
+    socket.on('add-message', (message) => {
+        io.emit('message', { text: message.text, from: socket.nickname, created: new Date() });
+    });
+
+    
+
+});
