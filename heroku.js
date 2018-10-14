736a8d6da44ac90a6367ace037dac4f59cec13a3
cuongdqpayment <cuongdqpayment@gmail.com>
2018-10-14 15:51:07 +0700

    heroku https

--- heroku.js
+++ heroku.js
@@ -24,20 +24,27 @@ function isValid(tk) {
     }
 }
 
+var request_count = 0;
+var requets_err = 0;
+var request_token = 0;
+
 // middleware kiem tra token truoc khi cho ket noi
-io.use((socket, next) => {
-    
+io.use((socket, next) => {    
+    var ip = socket.handshake.headers["x-forwarded-for"];
+    var user_agent = socket.handshake.headers['user-agent'];
+
     //Lay dia chi ip yeu cau tu dau
-    console.log(
-        'Client request ip: ' +socket.handshake.headers["x-forwarded-for"]
-        + ', agent: ' +socket.handshake.headers['user-agent']);
+    console.log( 'Request('+ ++request_count +') ip: ' + ip + ', agent: ' + user_agent);
     //console.log(socket.handshake.query);
     //chuoi handshake.query la sau dau ?
     let token = socket.handshake.query.token;
 
-    if (isValid(token)) {
+    //neu client khong co ip, khong khai bao moi truong thi se bi reject khong cho vao phong chat
+    if (isValid(token)&&ip!=''&&user_agent!='') {
       return next();
     }
+
+    requets_err++;
     return next(new Error('authentication error'));
   });
   
@@ -50,13 +57,36 @@ server.listen(port, function () {
 
 //lang nghe client ket noi len server
 io.on("connection", function(socket){
+    //moi khi co nguoi request io thanh cong voi token quy dinh
     //ghi lai token requet vao day de biet ho truy cap nhom lam viec nao
     socket.TokenRequest=socket.handshake.query.token;
     //hien chi cho phep nhom token=cuongdq moi vao duoc day
-    console.log("Co "+socket.TokenRequest+" ket noi len server: " + socket.id + ' ' + JSON.stringify(socket.request.connection._peername));  
+    console.log("Seq ("+ ++request_token +") connecting: " + socket.id + ' ' + JSON.stringify(socket.request.connection._peername));  
+
+    //thong bao cho trang quan tri biet tinh hinh hoat dong tong quat cua he thong
+    socket.emit('server-send-admin-info', { request_count: request_count,
+                                            request_token: request_token,
+                                            requets_err: requets_err });
    
+    socket.on("create-room",(roomId)=>{
+        //join vao mot room ten la data
+        socket.join(roomId);
+        socket.ROOM = roomId;
+        //liet ke cac room dang co
+        //consol.log(socket.adapter.rooms);
+        var rooms=[];
+        for (i in socket.adapter.rooms){
+            rooms.push(r);
+        }
+        //gui toan bo danh sach rooms cho all user
+        io.sockets.emit("server-send-rooms",rooms);
+        //gui cho chinh socket vua tao
+        socket.emit("server-send-room-socket",data);
+
+    })
+                                            
     socket.on("disconnect",function(){
-        console.log("Ngat " +socket.TokenRequest +" ket noi: " + socket.id);  
+        console.log("Disconnect seq(" + request_token-- +"): " + socket.id);  
         io.emit('users-changed', { user: socket.nickname, event: 'left' });
     });
 
@@ -70,6 +100,11 @@ io.on("connection", function(socket){
         io.emit('message', { text: message.text, from: socket.nickname, created: new Date() });
     });
 
+    //su kien client emit room
+    socket.on("user-chat-room",(data)=>{
+        //chi gui noi dung cho phong chat thoi
+        io.sockets.in(socket.ROOM).emit("server-send-user-room",data);
+    })
 
 });
 
